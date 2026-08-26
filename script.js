02efe8fc7a332989d004ecf1d1bc1c110236f917
Isabella Vilar <100245811+isabellavilar@users.noreply.github.com>
2026-08-26 15:56:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-
-
+const forms = document.querySelector('form'); // Seleciona o forms do html - contato
 forms.addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Não recarregar sozinho
 
-    let nome = document.getElementById('nome').value;
+//Pegar o que foi digitado dentro dos campos
+    let nome = document.getElementById('nome').value; 
     let email = document.getElementById('email').value;
     let mensagem = document.getElementById('mensagem').value;
 
+    //Alerta pra ver se funcionou o envio da mensagem e o email
     if (email.includes('@') && email.includes('.')) {
         alert("Mensagem enviada com sucesso!");
 
